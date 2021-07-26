1589fd3163d0ba23b11bf9758920105c6f62446b
HyunJae.Im <hjstylelu@gmail.com>
2021-07-26 22:34:43 +0900

    2021.07.26 Baek2750.cpp

---
+++ algorithm/Baek2750.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    auto nInput = 0;
+
+    cin >> nInput;
+
+    list<int> list;
+
+    auto nNumber = 0;
+    for(auto i = 0 ; i < nInput ; ++i)
+    {
+        cin >> nNumber;
+        list.push_back(nNumber);
+    }
+
+    list.sort();
+
+    for(auto i = 0 ; i < nInput ; ++i)
+    {
+        cout << list.front() << "\n";
+        list.pop_front();
+    }
+
+    return 0;
+}
