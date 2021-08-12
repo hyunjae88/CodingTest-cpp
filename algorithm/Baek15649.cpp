5a2d31096b899618a565cea1c867bc8ba12d418f
HyunJae.Im <hjstylelu@gmail.com>
2021-08-12 13:07:20 +0900

    2021.08.12 Add some Problems

---
+++ algorithm/Baek15649.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+
+int N, M = 0;
+
+int answer[MAX] = {0};
+bool isUsed[MAX] = {false};
+
+void backTrack(int level)
+{
+    if(level == M)
+    {
+        for(int i = 0 ; i < M ; ++i)
+        {
+            cout << answer[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    for(int i = 1 ; i <= N; ++i)
+    {
+        if(isUsed[i]) continue;
+        
+        isUsed[i] = true;
+        answer[level] = i;
+        backTrack(level + 1);
+        isUsed[i] = false;
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    
+    backTrack(0);
+    return 0;
+}
---
+++ algorithm/Baek15650.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+
+int N, M = 0;
+
+int answer[MAX] = {0};
+bool isUsed[MAX] = {false};
+
+void backTrack(int level)
+{
+    if(level == M)
+    {
+        for(int i = 0 ; i < M ; ++i)
+        {
+            cout << answer[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i = 1 ; i <= N ; ++i)
+    {
+        if(isUsed[i])   continue;
+
+        if( level!=0 ){
+            if(i < answer[level-1])
+            {
+                continue;
+            }
+        }
+        isUsed[i] = true;
+        answer[level] = i;
+        backTrack(level+1);
+        isUsed[i] = false;
+    }
+
+}
+
+int main()
+{
+    cin >> N >> M;
+
+    backTrack(0);
+
+    return 0;
+}
